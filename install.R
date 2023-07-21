1b6fc74f214faaf3d700219db6515c84e16938dd
BastienGrassetIRD <bastien.grasset@ird.fr>
2023-07-21 16:56:58 +0400

    simplified installing packages

--- install.R
+++ install.R
@@ -1,17 +1,40 @@
-package <- jsonlite::read_json('./srv/tunaatlas_pie_map_shiny/package.json')
-invisible(lapply(package$dependencies, function(pkg){
-  from <- 'cran'
-  pkg_installer <- remotes::install_version
-  if(!is.null(pkg$from)){
-    from <- pkg$from
-    pkg_installer <- try(eval(parse(text=paste0("remotes::install_",from))))
+
+packages <- jsonlite::read_json("package.json")
+
+
+for (package_info in packages$dependencies) {
+  pkg <- package_info$package
+  package_version <- package_info$version
+  package_repos <- package_info$repos
+  
+  # Install and load the package (if not already installed)
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, version = package_version, repos = package_repos)
+    require(pkg, character.only = TRUE)
   }
-  if(class(pkg_installer)[1] == "try-error") return(NULL)
-  version <- ""
-  if(!is.null(pkg$version)) version <- paste0("[",pkg$version,"]")
-  cat(sprintf("Install package '%s' %s from '%s'\n", pkg$package, version, from))
-  pkg_args <- pkg[!sapply(names(pkg), function(x){x %in% c("from","dependencies")})]
-  pkg_deps <- NA
-  if(!is.null(pkg$dependencies)) pkg_deps <- pkg$dependencies
-  do.call(pkg_installer, c(pkg_args, dependencies = pkg_deps))
-}))
+}
+
+# # Load packages
+# invisible(lapply(packages$dependencies, function(pkg) {
+#   browser()
+#   if (!requireNamespace(pkg$package, quietly = TRUE)) {
+#     from <- 'cran'
+#     pkg_installer <- remotes::install_version
+#     if (!is.null(pkg$from)) {
+#       from <- pkg$from
+#       pkg_installer <- try(eval(parse(text = paste0("remotes::install_", from))))
+#     }
+#     if (class(pkg_installer)[1] == "try-error") return(NULL)
+#     version <- ""
+#     if (!is.null(pkg$version)) version <- paste0("[", pkg$version, "]")
+#     cat(sprintf("Installing package '%s' %s from '%s'\n", pkg$package, version, from))
+#     pkg_args <- pkg[!names(pkg) %in% c("package", "from", "dependencies")]
+#     pkg_deps <- NULL
+#     if (!is.null(pkg$dependencies)) pkg_deps <- pkg$dependencies
+#     do.call(pkg_installer, c(pkg_args, dependencies = pkg_deps))
+#   }
+# }))
+# 
+# 
+# 
+# 

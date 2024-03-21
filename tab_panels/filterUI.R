c842408e7b50b3ec708892497f6f96108e2aa998
BastienGrassetIRD <bastien.grasset@ird.fr>
2024-03-21 16:10:08 +0400

    new pacakges

--- tab_panels/filterUI.R
+++ tab_panels/filterUI.R
@@ -3,7 +3,7 @@ filterUI <- function(id) {
   tagList(
     tags$head(includeCSS("https://raw.githubusercontent.com/eparker12/nCoV_tracker/master/styles.css")),
     h2("Select filters to customize indicators"),
-    selectInput(ns("dataset"), "Dataset", choices = NULL),
+    selectInput(ns("dataset"), "Dataset", selected = default_dataset, choices = NULL),
     selectInput(ns("gridtype"), "Resolution", choices = NULL),
     sliderInput(ns("year"), "Select period of interest:", min = 1950, max = 2025, value = c(1950, 2025)),
     tags$br(),

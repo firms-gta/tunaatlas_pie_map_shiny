e63bc2920ed176d97b2921c8919e0c9dbd5c4d5a
BastienGrassetIRD <bastien.grasset@ird.fr>
2024-11-19 17:09:00 +0400

    Using downloader

--- update_data.R
+++ update_data.R
@@ -1,4 +1,6 @@
 require(readr)
+require(utils)
+require(downloader)
 
 # Read the DOI file
 DOI <- read_csv("data/DOI.csv")
@@ -7,25 +9,32 @@ DOI <- read_csv("data/DOI.csv")
 options(timeout = 6000)
 
 # Function to download files using curl
-download_with_curl <- function(doi, filename, data_dir = "data") {
-  # Ensure the data directory exists
+download_source_file <- function(doi, filename, data_dir = "data") {
+  # S'assurer que le répertoire des données existe
   if (!dir.exists(data_dir)) dir.create(data_dir)
   
-  # Construct the URL from the DOI
-  url <- paste0("https://doi.org/", doi)
+  record_id <- sub("10\\.5281/zenodo\\.", "", doi)
   
-  # Destination file path
+  # Construire l'URL brute
+  raw_url <- paste0("https://zenodo.org/record/", record_id, "/files/", filename, "?download=1")
+  
+  # Encoder les caractères spéciaux dans l'URL
+  encoded_url <- URLencode(raw_url)
+  
+  # Chemin de destination
   destfile <- file.path(data_dir, filename)
   
-  # Attempt the download
+  # Télécharger le fichier
   tryCatch({
-    curl::curl_download(url = url, destfile = destfile)
+    downloader::download(encoded_url, destfile, mode = "wb")
+    message(sprintf("File '%s' downloaded successfully.", filename))
     
-    # Check if the file was successfully downloaded
+    
+    # Vérifier si le fichier a été correctement téléchargé
     if (file.exists(destfile)) {
-      message(sprintf("File '%s' downloaded successfully with curl", filename))
+      message(sprintf("File '%s' downloaded successfully from DOI: %s", filename, doi))
     } else {
-      stop(sprintf("File '%s' could not be downloaded with curl", filename))
+      stop(sprintf("File '%s' could not be downloaded from DOI: %s", filename, doi))
     }
   }, error = function(e) {
     message(sprintf("Curl download failed for file '%s': %s", filename, e$message))
@@ -39,7 +48,7 @@ lapply(1:nrow(DOI), function(i) {
   
   # Check if the file already exists
   if (!file.exists(filepath)) {
-    download_with_curl(doi = DOI$DOI[i], filename = DOI$Filename[i])
+    download_source_file(doi = DOI$DOI[i], filename = DOI$Filename[i])
   } else {
     message(sprintf("File '%s' already exists. Skipping download.", DOI$Filename[i]))
   }

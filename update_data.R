dc73314e3f3ae3d93f4c611292dc683ce94a1afa
bastienird <bastien.grasset@ird.fr>
2025-06-26 09:43:19 +0000

    fix: minor library call for docker

--- update_data.R
+++ update_data.R
@@ -60,6 +60,7 @@ if(!file.exists(cl_areal_grid_path)| !file.exists("data/centroids.qs")){
   if (!file.exists(cwp_grid_file) ) {
     stop("cl_areal_grid.csv not found in inst/extdata - run data-raw/download_codelists.R")
   }
+  library(dplyr)
   shp_raw <- sf::st_read(cwp_grid_file, show_col_types = FALSE)
   shapefile.fix <- sf::st_as_sf(shp_raw, wkt = "geom_wkt", crs = 4326)
   shapefile.fix <- dplyr::rename(shapefile.fix,

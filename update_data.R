3b38f173317435a1aceb37598f9387315daa0d9c
bastien.grasset_ird.fr <bastien.grasset@ird.fr>
2025-09-23 15:41:35 +0400

    feature: adding multiples files in docker image with reduced size

--- update_data.R
+++ update_data.R
@@ -23,11 +23,12 @@ download_and_rename <- function(doi, filename, data_dir = "data") {
   
   raw_path    <- file.path(data_dir, filename)
   renamed     <- file.path(data_dir, paste0(base, "_", record_id, ".", ext))
+  updated     <- file.path(data_dir, paste0(base, "_", record_id, "_updated.qs"))
   
   # 1) Si déjà renommé, on ne fait rien
-  if (file.exists(renamed)) {
+  if (file.exists(renamed) | file.exists(updated)) {
     message("📦 found renamed file: ", renamed)
-    return(renamed)
+    return(updated)
   }
   
   # 2) Si le brut existe, on le renomme
